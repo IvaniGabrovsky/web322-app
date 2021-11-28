3a0720339abf61b1aa7163c5bdf63fe0ce8a214b
Ivan Gabrovsky <ivangabrovsky@Ivans-MacBook-Pro.local>
2021-11-28 14:46:06 -0500

    added validation to check if password is correct

--- data-service-auth.js
+++ data-service-auth.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 //const { mapFinderOptions } = require("sequelize/types/lib/utils");
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
@@ -34,18 +35,27 @@ module.exports.registerUser = function (userData) {
     if (userData.password !== userData.password2) {
       reject(`Passwords do not match`);
     }
-    let newUser = new User(userData);
-    newUser.save((err) => {
-      if (err && err === 11000) {
-        // there was an error
-        reject(`User Name already taken`);
-      } else if (err && err !== 11000) {
-        // there was an error
-        reject(`There was an error creating the user: ${err}`);
-      } else if (!err) {
-        resolve(userData);
-      }
-    });
+    bcrypt
+      .genSalt(10) // Generate a "salt" using 10 rounds
+      .then((salt) => bcrypt.hash(userData.password, salt)) // encrypt the password: "myPassword123"
+      .then((hash) => {
+        userData.password = hash;
+        let newUser = new User(userData);
+        newUser.save((err) => {
+          if (err && err === 11000) {
+            // there was an error
+            reject(`User Name already taken`);
+          } else if (err && err !== 11000) {
+            // there was an error
+            reject(`There was an error creating the user: ${err}`);
+          } else if (!err) {
+            resolve(userData);
+          }
+        });
+      })
+      .catch((err) => {
+        reject(`Error: ${err}`); // Show any errors that occurred during the process
+      });
   });
 };
 
@@ -57,30 +67,38 @@ module.exports.checkUser = function (userData) {
         usersObj = users.map((value) => value.toObject());
         if (usersObj.length === 0) {
           reject(`Unable to find user: ${userData.userName}`);
-        } else if (usersObj[0].password.trim() !== userData.password.trim()) {
-          reject(`Incorrect Password for user: ${userData.userName}`);
-        } else {
-          // save to user history
-          if (!usersObj[0].loginHistory) {
-            usersObj[0].loginHistory = [];
-          }
-          usersObj[0].loginHistory.push({
-            dateTime: new Date().toString(),
-            userAgent: userData.userAgent,
-          });
-          User.updateOne(
-            { userName: userData.userName },
-            { $set: { loginHistory: usersObj[0].loginHistory } }
-          )
-            .exec()
-            .then(() => {
-              // resolve promise
-              resolve(usersObj[0]);
-            })
-            .catch((err) => {
-              reject(`There was an error verifying the user: ${err}`);
-            });
         }
+        bcrypt
+          .compare(userData.password, usersObj[0].password)
+          .then((valid) => {
+            // save to user history
+            if (valid) {
+              if (!usersObj[0].loginHistory) {
+                usersObj[0].loginHistory = [];
+              }
+              usersObj[0].loginHistory.push({
+                dateTime: new Date().toString(),
+                userAgent: userData.userAgent,
+              });
+              User.updateOne(
+                { userName: userData.userName },
+                { $set: { loginHistory: usersObj[0].loginHistory } }
+              )
+                .exec()
+                .then(() => {
+                  // resolve promise
+                  resolve(usersObj[0]);
+                })
+                .catch((err) => {
+                  reject(`There was an error verifying the user: ${err}`);
+                });
+            } else {
+              reject(`Incorrect Password for user: ${userData.userName}`);
+            }
+          })
+          .catch(() => {
+            reject(`Incorrect Password for user: ${userData.userName}`);
+          });
       })
       .catch(() => {
         reject(`Unable to find user: ${userData.userName}`);
