5b6088be07cff0679fb2cef873af038509b71dfb
Ivan Gabrovsky <ivangabrovsky@Ivans-MacBook-Pro.local>
2021-11-29 17:14:34 -0500

    increased session length

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.use(
   clientSessions({
     cookieName: "session", // this is the object name that will be added to 'req'
     secret: "web322", // this should be a long un-guessable string.
-    duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
-    activeDuration: 1000 * 60, // the session will be extended by this many ms each request (1 minute)
+    duration: 30 * 60 * 1000, // duration of the session in milliseconds
+    activeDuration: 15 * 1000 * 60, // the session will be extended by this many ms each request
   })
 );
 
@@ -427,7 +427,6 @@ app.post("/login", function (req, res) {
       req.session.user = {
         userName: user.userName, // authenticated user's userName
         email: user.email, // authenticated user's email
-        loginHistory: user.loginHistory, // authenticated user's loginHistory
       };
       res.redirect("/employees");
     })
